7e474740ca1d7ebab9c3d4e90a48f36bf67ee6f0
MarcosPixel <marcos@pixelratio.es>
2022-04-14 19:11:13 +0200

    [+]ADT vector [+] array's copy and move constructors [+] list's copy and move constructors

--- mb_adt.h
+++ mb_adt.h
@@ -17,10 +17,34 @@ template<class T>
 class List {
   
  public:
+ 
   List() { count_ = 0; head_ = nullptr; tail_ = nullptr; }
-  //TODO: copy constructors
-  List(const List &other) { count_ = other.count_; head_ = other.head_; tail_ = other.tail_; }
+  
   ~List() { clear(); }
+  
+  List(const List &other) 
+  { 
+    copy(other);
+  }
+    
+  const List<T>& operator=(const List<T>& other) 
+  {
+    copy(other);
+  
+    return *this;
+  }
+    
+  List(List &&other) 
+  { 
+    move(std::move(other));
+  }
+  
+  const List<T>& operator=(List<T>&& other) 
+  {
+    move(std::move(other));
+  
+    return *this;
+  }
   
   void add(const T &item)
   {
@@ -188,15 +212,6 @@ class List {
     return *this;
   }
   
-  const List<T>& operator=(const List<T>& other) 
-  {
-    count_ = other.count_; 
-    head_ = other.head_; 
-    tail_ = other.tail_;
-    
-    return *this;
-  };
-  
   void printList()
   {
     printf("\n\nList Debug Print %p\n", this);
@@ -278,7 +293,7 @@ class List {
     ++count_;
   }
   
-  void insertInternal(Node *new_entry, int32_t position)
+  void insertInternal(Node *new_entry, uint32_t position)
   {
     
     if(position == 0)
@@ -299,11 +314,257 @@ class List {
     
     ++count_;
   }
+  
+  void copy(const List& other)
+  {
+    clear();
+    
+    for(Node *it = other.head_; it != nullptr; it = it->next)
+      add(it->data);
+    
+    count_ = other.count_;
+  }  
+  
+  void move(List&& other)
+  {
+    clear();
+    
+    head_ = other.head_;
+    tail_ = other.tail_;
+    count_ = other.count_;
+    
+    other.head_ = nullptr;
+    other.tail_ = nullptr;
+    other.count_ = 0;
+  }
  
 };
 
 template<class T>
 class LockeableList : public List<T>, public ThreadLocks { };
+
+template<class T>
+class Vector {
+  
+ public:
+ 
+  Vector() { storage_ = nullptr; capacity_ = 0; count_ = 0; }
+  
+  ~Vector() { destroy(); }
+  
+  Vector(const Vector& other)
+  {
+    copy(other);
+  }
+  
+  Vector<T>& operator=(const Vector& other)
+  {
+    copy(other);
+    return *this;
+  }
+  
+  Vector(Vector&& other)
+  {
+    move(std::move(other));
+  }
+  
+  Vector<T>& operator=(Vector&& other)
+  {
+    move(std::move(other));
+    return *this;
+  }
+  
+  void init(uint32_t capacity) 
+  {
+    destroy();
+    //Purposely not calling any constructor.
+    storage_ = (T*)calloc(capacity, sizeof(T));
+    
+    capacity_ = capacity;
+    count_ = 0;
+  }
+  
+  void destroy()
+  {
+    if(storage_ != nullptr)
+    {
+      clear();
+      free(storage_);
+    }
+    
+    storage_ = nullptr;
+    capacity_ = 0;
+    count_ = 0;
+  }
+  
+  void clear()
+  {
+    for(uint32_t i = 0; i < count_; ++i)
+    {
+      storage_[i].~T();
+    }
+    
+    memset(storage_, 0, sizeof(T) * count_);
+    
+    count_ = 0;
+  }
+  
+  void resize(uint32_t new_size)
+  {
+    if(new_size == capacity_)
+      return;
+    
+    T* new_storage = (T*)malloc(new_size*sizeof(T));
+    
+    if(new_size >= capacity_)
+    {
+      std::memcpy(new_storage, storage_, sizeof(T) * capacity_);
+      std::memset(new_storage + capacity_, 0, sizeof(T) * (new_size - capacity_));
+      memset(storage_, 0, sizeof(T) * count_);
+    }
+    else
+    {
+      std::memcpy(new_storage, storage_, sizeof(T) * new_size);
+      
+      for(uint32_t i = new_size; i < count_; ++i)
+      {
+        storage_[i].~T();
+      }
+      
+      memset(storage_, 0, sizeof(T) * count_);
+      
+      count_ = new_size < count_ ? new_size : count_;
+    }
+    
+    free(storage_);
+    
+    storage_ = new_storage;
+    capacity_ = new_size;
+  }
+  
+  void add(const T &item)
+  {
+    if(count_ >= capacity_)
+      return;
+    
+    storage_[count_] = item;
+    ++count_;
+  }  
+  
+  void add(T &&item)
+  {
+    if(count_ >= capacity_)
+      return;
+    
+    storage_[count_] = std::move(item);
+    ++count_;
+  }
+  
+  void remove(const T &item)
+  {
+    bool found = false;
+    uint32_t i;
+    
+    for(i = 0; i < count_ && !found; ++i)
+    {
+      if(storage_[i] == item)
+      {
+        removeAt(i);
+        found = true;
+      }
+    }
+  }
+  
+  void removeAt(uint32_t index)
+  {
+    if(index >= count_)
+      return;
+    
+    storage_[index].~T();
+    
+    uint32_t data_to_move = count_ - index - 1;
+    
+    memcpy(storage_ + index, storage_ + index + 1, sizeof(T) * data_to_move);
+    memset(storage_ + (count_ - 1), 0, sizeof(T));
+    
+    --count_;
+  }
+
+  void debugPrint()
+  {
+    printf("\n\nVector %p, storage %p, capacity %d, count %d\n", this, storage_, capacity_, count_);
+    printf("\nContent:\n\n");
+    
+    for(uint32_t i = 0; i < count_; ++i)
+    {
+      printf(" %p ", storage_ + i);
+    }
+  }
+
+  uint32_t capacity() { return capacity_; }
+  uint32_t count() { return count_; }
+  
+  Vector<T>& operator+=(const T &item)
+  {
+    add(item);
+    return *this;
+  }  
+  
+  Vector<T>& operator+=(T &&item)
+  {
+    add(std::move(item));
+    return *this;
+  }  
+  
+  Vector<T>& operator-=(const T &item)
+  {
+    remove(item);
+    return *this;
+  }
+  
+ private:
+ 
+  T* storage_;
+  uint32_t capacity_;
+  uint32_t count_;
+  
+  void copy(const Vector &other)
+  {
+    destroy();
+    
+    if(other.capacity_ == 0)
+      return;
+    
+    storage_ = (T*)malloc(sizeof(T)*other.capacity_);
+    
+    for(uint32_t i = 0; i < other.count_; ++i)
+    {
+      storage_[i] = other.storage_[i];
+    }
+    
+    capacity_ = other.capacity_;
+    count_ = other.count_;
+    
+    memset(storage_ + count_, 0, sizeof(T) * (capacity_ - count_));
+  }
+  
+  void move(Vector &&other)
+  {
+    destroy();
+    
+    storage_ = other.storage_;
+    capacity_ = other.capacity_;
+    count_ = other.count_;
+    
+    other.storage_ = nullptr;
+    other.capacity_ = 0;
+    other.count_ = 0;
+  }
+  
+};
+
+template<class T>
+class LockeableVector : public Vector<T>, public ThreadLocks { };
 
 template<class T>
 class Array {
@@ -316,26 +577,66 @@ class Array {
     lenght_ = 0;
   }
  
-  Array(int32_t size) 
+  Array(uint32_t size, bool clear_values = false) 
   {
-    data_ = new T[size];
-    lenght_ = size;
+    init(size, clear_values);
   }
   
   ~Array() 
   { 
-    if(data_ != nullptr) { delete[] data_; }
+    clear();
+  }
+  
+  Array(const Array& other) 
+  {
+    copy(other);
+  }
+  
+  const Array<T>& operator=(const Array<T>& other) 
+  {
+    copy(other);
+    
+    return *this;
+  }  
+  
+  Array(Array&& other) 
+  {
+    move(std::move(other));
+  }
+  
+  const Array<T>& operator=(Array<T>&& other) 
+  {
+    move(std::move(other));
+  
+    return *this;
+  }
+  
+  void init(uint32_t size, bool clear_values = false)
+  {
+    clear();
+    
+    data_ = new T[size];
+    lenght_ = size;
+    
+    if(clear_values)
+      memset(0);
+  }
+  
+  void clear()
+  {
+    if(data_ != nullptr)
+      delete[] data_;
     lenght_ = 0;
   }
-  //TODO: these constructors + move
-  Array(const Array& other) = delete;
-  const Array<T>& operator=(const Array<T>& other) = delete;
   
-  void resize(int32_t size)
+  void resize(uint32_t size)
   {
+    if(size == lenght_)
+      return;
+    
     T* new_buffer = new T[size];
     
-    int32_t cpy_lenght = size < lenght_ ? size : lenght_;
+    uint32_t cpy_lenght = size < lenght_ ? size : lenght_;
 
     std::memcpy(new_buffer, data_, sizeof(T) * cpy_lenght);
     std::memset(data_, 0, sizeof(T) * cpy_lenght);
@@ -349,32 +650,59 @@ class Array {
   
   void traverse(void(*func)(T&))
   {
-    for(int32_t i = 0; i < lenght_; ++i)
+    for(uint32_t i = 0; i < lenght_; ++i)
     {
       func(data_[i]);
     }
   }
   
-  int32_t lenght(){ return lenght_; }
+  uint32_t lenght(){ return lenght_; }
   
-  T& operator[](int32_t index)
+  T& operator[](uint32_t index)
   {     
     return data_[index];
   }
 
  private:
-  int32_t lenght_;
+ 
+  uint32_t lenght_;
   T *data_; 
+  
+  void copy(const Array& other)
+  {
+    clear();
+    
+    data_ = new T[other.lenght_];
+    lenght_ = other.lenght_;
+    
+    for(uint32_t i = 0; i < lenght_; ++i)
+      data_[i] = other.data_[i];
+  }  
+  
+  void move(Array&& other)
+  {
+    clear();
+    
+    data_ = other.data_;
+    lenght_ = other.lenght_;
+    
+    other.data_ = nullptr;
+    other.lenght_ = 0;
+  }
+  
 };
 
 template<class T>
 class LockeableArray : public Array<T>, public ThreadLocks { };
 
 class ThreadLocks {
+  
  public:
  
   ThreadLocks() { spinLock_ = 0; }
+  
   ThreadLocks(const ThreadLocks &other) { }
+  
   ThreadLocks(ThreadLocks &&other) { }
   
   ThreadLocks& operator=(ThreadLocks&& other) {
@@ -401,6 +729,7 @@ class ThreadLocks {
   void spinUnlock() { spinLock_.store(0); }
   
  private:
+ 
   std::mutex lock_;
   std::atomic<int> spinLock_;
 };
